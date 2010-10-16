fadbbfdc80ec08bf01cdc31cd077853025191ae4
Gordon Franke <info@nevalon.de>
2010-10-16 20:41:49 +0200

    cleanup and change to use http_build_query function

--- lib/listener/Geolocatable.php
+++ lib/listener/Geolocatable.php
@@ -1,17 +1,19 @@
 <?php
 
-// 
-//  Locatable.php
-//  Locatable Extension
-//  
-//  Created by Brent Shaffer on 2008-12-22.
-//  Copyright 2008 Centre{source}. All rights reserved.
-// 
-
+/**
+ * Easily refresh geo cordinates based on a specified set of fields
+ *
+ * @package    Locatable_Extension
+ * @subpackage listener
+ * @author     Brent Shaffer
+ * @copyright  Copyright (c) 2008 Centre{source}, Brent Shaffer 2008-12-22. All rights reserved.
+ */
 class Doctrine_Template_Listener_Geolocatable extends Doctrine_Record_Listener
 {
   /**
    * Array of locatable options
+   *
+   * @var array
    */  
   protected $_options = array();
 
@@ -19,9 +21,9 @@ class Doctrine_Template_Listener_Geolocatable extends Doctrine_Record_Listener
   /**
    * Constructor for Locatable Template
    *
-   * @param array $options 
+   * @param array $options
+   *
    * @return void
-   * @author Brent Shaffer
    */  
   public function __construct(array $options)
   {
@@ -33,26 +35,31 @@ class Doctrine_Template_Listener_Geolocatable extends Doctrine_Record_Listener
    * Set the geocodes automatically when a new locatable object is created
    *
    * @param Doctrine_Event $event
+   *
    * @return void
-   * @author Brent Shaffer
    */
   public function preInsert(Doctrine_Event $event)
   {
-    $object = $event->getInvoker();
-		$object->refreshGeocodes();
+    $event
+      ->getInvoker()
+      ->refreshGeocodes();
   }
   
   /**
    * Set the geocodes automatically when a locatable object's locatable fields are modified
    *
    * @param Doctrine_Event $event
+   *
    * @return void
-   * @author Brent Shaffer
    */
   public function preSave(Doctrine_Event $event)
   {
     $object = $event->getInvoker();
+
     $modified = array_keys($object->getModified());
-    if (array_intersect($this->_options['fields'], $modified)) $object->refreshGeocodes();
+    if (array_intersect($this->_options['fields'], $modified))
+    {
+      $object->refreshGeocodes();
+    }
   }
 }
